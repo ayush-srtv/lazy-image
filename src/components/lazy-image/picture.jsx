dd67b8dc6c425999aa88f075cddb3dcbcf1edf0c
Ayush Srivastava <ayushs.1992@gmail.com>
2020-08-27 22:39:28 +0530

    Added states for images, observers, refs.

--- src/components/lazy-image/picture.jsx
+++ src/components/lazy-image/picture.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Picture({ sources, ...props }) {
+  const [images, setImages] = useState([]);
+  const [observers, setObservers] = useState([]);
+  const [imagesRefs, setImagesRefs] = useState([]);
+  useEffect(() => {}, [imagesRefs, images]);
+  const handleImageRef = (ref, index) => {
+    const refs = [...imagesRefs];
+    refs[index] = ref;
+    setImagesRefs(refs);
+  };
+
   return (
     <picture {...props}>
-      {sources.map(({ type: Source = "source", ...rest }) => (
-        <Source {...rest} />
+      {sources.map(({ type: Source = "source", ...rest }, index) => (
+        <Source {...rest} ref={(ref) => handleImageRef(ref, index)} />
       ))}
     </picture>
   );
