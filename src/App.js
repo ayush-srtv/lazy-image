f554adde784e1bc8f7f67f3381cf98a54e1621f4
Ayush Srivastava <ayushs.1992@gmail.com>
2020-08-28 21:28:39 +0530

    removed images.js file.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Image from "./components/lazy-image";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
+      <Image src="https://images.unsplash.com/photo-1587115507733-b0ed701aa7ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
